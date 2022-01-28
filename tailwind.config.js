c759d32922117e22aafac319e0531726928c0c65
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-01-28 21:50:55 +0600

    Finish Header sectins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,20 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
   theme: {
-    screens: {
-      sm: "480px",
-      md: "768px",
-      lg: "976px",
-      xl: "1440px",
-    },
-    colors: {
-      primary: {
-        dark: "#28293E",
-        "light-dark": "#391400",
-        "dark-gray": "rgba(57, 20, 0, 0.64)",
-      },
-      secondary: {
-        dark: "#EF6D58",
-        light: "#FDF0E9",
-      },
-      third: {
-        light: "FFFFFF",
+    extend: {
+      colors: {
+        primary: {
+          dark: "#28293E",
+          "light-dark": "#391400",
+          "dark-gray": "rgba(57, 20, 0, 0.64)",
+        },
+        secondary: {
+          dark: "#EF6D58",
+          light: "#FDF0E9",
+        },
+        light: "#FFFFFF",
         "light-gray": "rgba(255, 255, 255, 0.64)",
       },
     },
-    extend: {},
   },
   plugins: [],
 };
